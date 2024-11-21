761f40250394de159af248415187d8065799877a
KimKyuHyun <kim.kyuhyun@lumir.space>
2024-11-21 11:03:03 +0900

    fix: vercel.json 수정

--- src/modules/menu/menu.controller.ts
+++ src/modules/menu/menu.controller.ts
@@ -6,17 +6,29 @@ import {
   Patch,
   Param,
   Delete,
+  Req,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { MenuService } from './menu.service';
 import { CreateMenuDto } from './dto/create-menu.dto';
 import { UpdateMenuDto } from './dto/update-menu.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('menu')
 export class MenuController {
-  constructor(private readonly menuService: MenuService) {}
+  constructor(
+    private readonly menuService: MenuService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
-  getMenu() {
+  getMenu(@Req() req) {
+    if (
+      req.headers.get('Authorization') !==
+      `Bearer ${this.configService.get<string>('SUPABASE_URL')}`
+    ) {
+      throw new UnauthorizedException('비인가 요청');
+    }
     return this.menuService.handleCrolling();
   }
 }
